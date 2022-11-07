6430f1570da6cc7812588f79b5e786c4944c00f6
Julian Layen <layenjulian@gmail.com>
2022-11-07 14:30:18 +0100

    Update DB.sql

--- Documentation/DB.sql
+++ Documentation/DB.sql
@@ -1,8 +1,3 @@
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
 DROP TABLE IF EXISTS `booking`;
 CREATE TABLE `booking` (
   `slot_id` int NOT NULL,
